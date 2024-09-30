af242c7ae94821b2989dc4786f785fad93c0ad1d
VitokNodes <dudarvasya1999@gmail.com>
2024-09-30 04:30:27 +0200

    Update instal_rivalz.sh

--- Rivalz/instal_rivalz.sh
+++ Rivalz/instal_rivalz.sh
@@ -103,6 +103,6 @@ mv "$TMP_FILE" "$FILE"
 
 # Создание сессии screen и запуск ноды
 log_message 'Создание screen сессии и запуск ноды Rivalz...'
-screen -S rivalz_node -dm bash -c 'rivalz run'
+screen -S rivalz -dm bash -c 'rivalz run'
 
 log_message 'Установка завершена. Нода Rivalz запущена в screen сессии "rivalz_node".'

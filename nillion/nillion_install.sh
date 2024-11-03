2c8b397373d7c1e5abbf7c0f76d24452d376d6bb
NodEligible <dudarvasya1999@gmail.com>
2024-11-03 18:42:28 +0100

    Update nillion_install.sh

--- nillion/nillion_install.sh
+++ nillion/nillion_install.sh
@@ -17,7 +17,7 @@ bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     source $HOME/.bash_profile
 fi
-sleep 1 && curl -s https://api.nodes.guru/logo.sh | bash && sleep 1
+sleep 1 && curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash && sleep 1
 
 cd $HOME
 sudo install -m 0755 -d /etc/apt/keyrings

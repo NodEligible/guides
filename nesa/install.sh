cce2bdcd65372a0e76ae1267c87c43cb22300045
NodEligible <dudarvasya1999@gmail.com>
2024-11-04 13:07:43 +0100

    Update install.sh

--- nesa/install.sh
+++ nesa/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function colors {
+  GREEN="\e[32m"
+  NORMAL="\e[0m"
+}
 # Функция для отображения логотипа
 display_logo() {
 curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
@@ -8,16 +12,16 @@ curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/d
 # Отображение логотипа
 display_logo
 
-echo "${green}Установка Main${reset}"
+echo "${GREEN}Установка Main${NORMA}"
 bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/main.sh)
 
-echo "${green}Установка Ufw${reset}"
+echo "${GREEN}Установка Ufw${NORMA}"
 bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/ufw.sh)
 
-echo "${green}Установка Docker${reset}"
+echo "${GREEN}Установка Docker${NORMA}"
 bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/docker.sh)
 
-echo "${green}Установка Go${reset}"
+echo "${GREEN}Установка Go${NORMA}"
 bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/go.sh)
 
-echo "${green}Установка Nesa${reset}"
+echo "${GREEN}Установка Nesa${NORMA}"

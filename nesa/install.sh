13d013c647fff4fd5b6ae0b148343368eae9168d
NodEligible <dudarvasya1999@gmail.com>
2024-11-04 12:53:29 +0100

    Update install.sh

--- nesa/install.sh
+++ nesa/install.sh
@@ -8,16 +8,16 @@ curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/d
 # Отображение логотипа
 display_logo
 
-echo -e "\033[32mУстановка Main\033[0m"
+printf "\033[32mУстановка Main\033[0m"
 bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/main.sh)
 
-echo -e "\033[32mУстановка Ufw\033[0m"
+printf "\033[32mУстановка Ufw\033[0m"
 bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/ufw.sh)
 
-echo -e "\033[32mУстановка Docker\033[0m"
+printf "\033[32mУстановка Docker\033[0m"
 bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/docker.sh)
 
-echo -e "\033[32mУстановка Go\033[0m"
+printf "\033[32mУстановка Go\033[0m"
 bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/go.sh)
 
-echo -e "\033[32mУстановка Nesa\033[0m"'
+printf "\033[32mУстановка Nesa\033[0m"'

3508021e366b9e81cdf509e76a448df37a9f0055
NodEligible <dudarvasya1999@gmail.com>
2024-11-05 01:19:23 +0100

    Update install.sh

--- nesa/install.sh
+++ nesa/install.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 
+# Змінні для кольорів
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # Скидання кольору
 
-# Функция для отображения логотипа
-display_logo() {
-curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
-}
-
-# Отображение логотипа
-display_logo
-
-# Функція для виведення тексту зеленим кольором
-function green_text {
-    # Перевіряємо, чи підтримує термінал кольори
-    if tput setaf 1 &> /dev/null; then
-        tput setaf 2  # Встановлюємо зелений колір
-        echo "$1"     # Виводимо текст
-        tput sgr0     # Скидаємо колір до початкового
-    else
-        echo "$1"     # Якщо кольори не підтримуються, виводимо текст звичайним
-    fi
-}
-
-# Використання функції green_text для повідомлень
-green_text "Установка Docker..."
+# Повідомлення про початок встановлення
+echo -e "${GREEN}Установка Docker...${NC}"
 
 # Оновлення репозиторіїв і встановлення Docker
 sudo apt update -y
@@ -30,9 +14,7 @@ sudo apt install -y docker.io
 
 # Перевірка статусу встановлення
 if [ $? -eq 0 ]; then
-    green_text "Docker успішно встановлено!"
+    echo -e "${GREEN}Docker успішно встановлено!${NC}"
 else
-    tput setaf 1  # Червоний колір для помилки
-    echo "Помилка при встановленні Docker"
-    tput sgr0     # Скидаємо колір до початкового
+    echo -e "${RED}Помилка при встановленні Docker${NC}"
 fi

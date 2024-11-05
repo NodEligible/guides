2eb771330fc15ba0f6d9c8bc90201125341c19a2
NodEligible <dudarvasya1999@gmail.com>
2024-11-05 01:14:17 +0100

    Update install.sh

--- nesa/install.sh
+++ nesa/install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-function colors {
-  GREEN="\e[32m"
-  YELLOW="\e[33m"
-  RED="\e[39m"
-  NORMAL="\e[0m"
-}
 
 # Функция для отображения логотипа
 display_logo() {
@@ -15,7 +9,7 @@ curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/d
 # Отображение логотипа
 display_logo
 
-function install_Main
-        echo -e "${YELLOW}Устанавливаем Main${NORMAL}"
-        bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/main.sh)
-        echo -e "${GREEN}Докер уже установлен. Переходим на следующий шаг${NORMAL}"
+# Встановлюємо Docker
+echo "${green}Установка Docker${reset}"
+sudo apt update
+sudo apt install -y docker.io

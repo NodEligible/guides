cf4aebb97bedb94d0d7f6d1d9c06ee964260645c
NodEligible <dudarvasya1999@gmail.com>
2024-11-05 01:18:07 +0100

    Update install.sh

--- nesa/install.sh
+++ nesa/install.sh
@@ -9,11 +9,30 @@ curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/d
 # Отображение логотипа
 display_logo
 
-# Налаштовуємо кольори
-green=$(tput setaf 2)
-reset=$(tput sgr0)
+# Функція для виведення тексту зеленим кольором
+function green_text {
+    # Перевіряємо, чи підтримує термінал кольори
+    if tput setaf 1 &> /dev/null; then
+        tput setaf 2  # Встановлюємо зелений колір
+        echo "$1"     # Виводимо текст
+        tput sgr0     # Скидаємо колір до початкового
+    else
+        echo "$1"     # Якщо кольори не підтримуються, виводимо текст звичайним
+    fi
+}
 
-# Встановлюємо Docker
-echo "${green}Установка Docker${reset}"
-sudo apt update
+# Використання функції green_text для повідомлень
+green_text "Установка Docker..."
+
+# Оновлення репозиторіїв і встановлення Docker
+sudo apt update -y
 sudo apt install -y docker.io
+
+# Перевірка статусу встановлення
+if [ $? -eq 0 ]; then
+    green_text "Docker успішно встановлено!"
+else
+    tput setaf 1  # Червоний колір для помилки
+    echo "Помилка при встановленні Docker"
+    tput sgr0     # Скидаємо колір до початкового
+fi

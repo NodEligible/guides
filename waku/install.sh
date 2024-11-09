ce90acac1f7fe4c79846f4c84259a4809d2b743f
NodEligible <dudarvasya1999@gmail.com>
2024-11-10 00:18:55 +0100

    Update install.sh

--- waku/install.sh
+++ waku/install.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# Функция для отображения логотипа
 display_logo() {
 curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
 }
 
-# Отображение логотипа
 display_logo
 
-# Оголошення кольорів для використання в скрипті
 YELLOW='\e[0;33m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-NC='\033[0m' # Сброс цвета
+NC='\033[0m' 
 
-# Встановлення інструментів
 install_tools() {
   echo -e "${YELLOW}Установка Tools...${NC}" 
   sudo apt update && sudo apt install mc wget htop jq git -y
@@ -25,7 +21,6 @@ install_tools() {
   fi
 }
 
-# Встановлення Docker
 install_docker() {
   echo -e "${YELLOW}Установка Docker...${NC}"
   bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/docker.sh)
@@ -36,7 +31,6 @@ install_docker() {
   fi
 }
 
-# Встановлення UFW
 install_ufw() {
   echo -e "${YELLOW}Установка Ufw...${NC}" 
   bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/ufw.sh)
@@ -47,7 +41,6 @@ install_ufw() {
   fi
 }
 
-# Введення RPC URL
 read_sepolia_rpc() {
   if [ -z "$RPC_URL" ]; then
     echo -e "Введите ваш RPC Sepolia https url. Пример url'a - https://sepolia.infura.io/v3/ТУТ_ВАШ_КЛЮЧ"
@@ -55,7 +48,6 @@ read_sepolia_rpc() {
   fi
 }
 
-# Введення приватного ключа
 read_private_key() {
   if [ -z "$WAKU_PRIVATE_KEY" ]; then
     echo -e "Введите ваш приватник от ETH кошелека на котором есть как минимум 0.1 ETH в сети Sepolia"
@@ -63,7 +55,6 @@ read_private_key() {
   fi
 }
 
-# Введення пароля
 read_pass() {
   if [ -z "$WAKU_PASS" ]; then
     echo -e "Введите(придумайте) пароль который будет использваться для сетапа ноды"
@@ -71,12 +62,10 @@ read_pass() {
   fi
 }
 
-# Клонування репозиторію
 git_clone() {
   git clone https://github.com/waku-org/nwaku-compose
 }
 
-# Налаштування середовища
 setup_env() {
   cd nwaku-compose || exit
   cp .env.example .env
@@ -86,7 +75,6 @@ setup_env() {
   sed -i "s|RLN_RELAY_CRED_PASSWORD=.*|RLN_RELAY_CRED_PASSWORD=$WAKU_PASS|" .env
   sed -i "s|NWAKU_IMAGE=.*|NWAKU_IMAGE=wakuorg/nwaku:v0.33.0|" .env
 
-  # Меняем стандартный порт графаны
   sed -i 's/0\.0\.0\.0:3000:3000/0.0.0.0:3004:3000/g' docker-compose.yml
   sed -i 's/127\.0\.0\.1:4000:4000/0.0.0.0:4044:4000/g' docker-compose.yml
   sed -i 's/:5432:5432/:5444:5432/g' docker-compose.yml
@@ -94,12 +82,10 @@ setup_env() {
   bash register_rln.sh
 }
 
-# Запуск Docker контейнерів
 docker_compose_up() {
   docker compose -f docker-compose.yml up -d
 }
 
-# Інформація про команди для користувача
 echo_info() {
   echo -e "${GREEN}Для остановки ноды waku: ${NC}"
   echo -e "${YELLOW}   docker-compose -f $HOME/nwaku-compose/docker-compose.yml down${NC}"
@@ -115,7 +101,6 @@ echo_info() {
   echo -e "${YELLOW}   http://$ip_address:3004/d/yns_4vFVk/nwaku-monitoring${NC}"
 }
 
-# Основний процес встановлення
 main() {
   install_tools
   install_docker
@@ -129,5 +114,4 @@ main() {
   echo_info
 }
 
-# Виконання основного процесу
 main

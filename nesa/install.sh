2885fdee39416058ad4d51b867ace5b3621eeaf5
NodEligible <dudarvasya1999@gmail.com>
2024-11-10 20:14:53 +0100

    Update install.sh

--- nesa/install.sh
+++ nesa/install.sh
@@ -14,9 +14,9 @@ install_docker() {
   echo -e "${YELLOW}Установка Docker...${NC}"
   bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/docker.sh)
   if [ $? -eq 0 ]; then
-      echo -e "${GREEN}Docker успешно установлено!${NC}"
+     echo -e "${GREEN}Docker успешно установлено!${NC}"
   else
-      echo -e "${RED}Ошибка при установке Docker!${NC}"
+     echo -e "${RED}Ошибка при установке Docker!${NC}"
   fi
 }
 
@@ -25,9 +25,9 @@ install_main() {
   echo -e "${YELLOW}Установка Main...${NC}"
   bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/main.sh)
   if [ $? -eq 0 ]; then
-      echo -e "${GREEN}Main успешно установлен!${NC}"
+     echo -e "${GREEN}Main успешно установлен!${NC}"
   else
-      echo -e "${RED}Ошибка при установке Main!${NC}"
+     echo -e "${RED}Ошибка при установке Main!${NC}"
   fi
  }
   
@@ -36,30 +36,30 @@ install_ufw() {
   echo -e "${YELLOW}Установка Ufw...${NC}" 
   bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/ufw.sh)
   if [ $? -eq 0 ]; then
-      echo -e "${GREEN}Ufw успешно установлено!${NC}"
+     echo -e "${GREEN}Ufw успешно установлено!${NC}"
   else
-      echo -e "${RED}Ошибка при установке Ufw!${NC}"
+     echo -e "${RED}Ошибка при установке Ufw!${NC}"
   fi
 }
 
 # Установка Go
 install_go() {
-    echo -e "${YELLOW}Установка Go...${NC}"
-    bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/go.sh)
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}Go успешно установлен!${NC}"
-    else
-        echo -e "${RED}Ошибка при установке Go!${NC}"
-    fi
+   echo -e "${YELLOW}Установка Go...${NC}"
+   bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/go.sh)
+   if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Go успешно установлен!${NC}"
+   else
+      echo -e "${RED}Ошибка при установке Go!${NC}"
+   fi
 }    
 
 # Установка Nesa
 install_nesa() {
   echo -e "${YELLOW}Установка Nesa...${NC}"
   bash <(curl -s https://raw.githubusercontent.com/nesaorg/bootstrap/master/bootstrap.sh)
-if [ $? -eq 0 ]; then
-    echo -e "${GREEN}Nesa успешно установлена и запущена!${NC}"
-else
-    echo -e "${RED}Ошибка при установке Nesa!${NC}"
-fi
+  if [ $? -eq 0 ]; then
+     echo -e "${GREEN}Nesa успешно установлена и запущена!${NC}"
+  else
+     echo -e "${RED}Ошибка при установке Nesa!${NC}"
+  fi
 }

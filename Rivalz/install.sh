0e4c17d406b0cc98c2508a4eccf40e34d557fb5b
NodEligible <dudarvasya1999@gmail.com>
2024-11-29 21:55:40 +0100

    Update install.sh

--- Rivalz/install.sh
+++ Rivalz/install.sh
@@ -88,5 +88,5 @@ mv "$TMP_FILE" "$FILE"
 
 # Creating screen session and running Rivalz node
 echo -e "${GREEN}Создание screen сессии и запуск ноды Rivalz...${NC}"
-screen -S rivalz -dm bash -c 'rivalz run'
-screen -r rivalz
+screen -S rivalz -dm bash -c 'sleep 3; rivalz run'
+sleep 5 && screen -r rivalz

17c758a1875b0619ab099ba0a9c3e2d2d4d7f332
NodEligible <dudarvasya1999@gmail.com>
2024-11-29 11:55:33 +0100

    Update install.sh

--- hemi/install.sh
+++ hemi/install.sh
@@ -39,11 +39,11 @@ NC='\033[0m'
 echo -e "${YELLOW}Установка майнера Hemi!${NC}"
 
 cd $HOME
-wget https://github.com/hemilabs/heminetwork/releases/download/v0.5.0/heminetwork_v0.5.0_linux_amd64.tar.gz
+wget https://github.com/hemilabs/heminetwork/releases/download/v0.7.0/heminetwork_v0.7.0_linux_amd64.tar.gz
 
-tar -xvf heminetwork_v0.5.0_linux_amd64.tar.gz && rm heminetwork_v0.5.0_linux_amd64.tar.gz
-mv heminetwork_v0.5.0_linux_amd64 heminetwork
-rm -rf $HOME/heminetwork_v0.5.0_linux_amd64
+tar -xvf heminetwork_v0.7.0_linux_amd64.tar.gz && rm heminetwork_v0.7.0_linux_amd64.tar.gz
+mv heminetwork_v0.7.0_linux_amd64 heminetwork
+rm -rf $HOME/heminetwork_v0.7.0_linux_amd64
 
 echo -e "${YELLOW}Создание кошелька${NC}"
 

210096b7cfd4b4edd6685e5229c9f12b8568c210
NodEligible <dudarvasya1999@gmail.com>
2024-12-26 20:58:02 +0100

    Update random_chat.sh

--- gaianet/random_chat.sh
+++ gaianet/random_chat.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
-
 YELLOW='\e[0;33m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m
-
 
 install_dependencies() {
   echo -e "${YELLOW}Установка автоматического общения с AI ботом${NC}"

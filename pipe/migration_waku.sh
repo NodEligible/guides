70a1431a636242eb3bd7eb317f741c2627d89e78
NodEligible <dudarvasya1999@gmail.com>
2024-12-26 22:48:06 +0100

    Create migration_waku.sh

---
+++ pipe/migration_waku.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Відображення логотипу
+curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
+
+YELLOW='\e[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+cd $HOME/nwaku-compose
+
+docker compose down
+
+sed -i 's|127.0.0.1:8003:8003|127.0.0.1:8333:8003|' $HOME/nwaku-compose/docker-compose.yml
+
+docker compose up -d
+
+echo -e "${GREEN}Миграция завершена!${NC}"

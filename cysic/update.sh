e75822d8ca423dc4e100c12f17d946baf31f1a5d
NodEligible <dudarvasya1999@gmail.com>
2025-01-06 03:02:42 +0100

    Create update.sh

---
+++ cysic/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
+
+YELLOW='\e[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${YELLOW}Остановка ноди${NC}"
+pkill -f "./verifier"
+
+sleep 2
+
+echo -e "${YELLOW}Удаляем двоичный файл${NC}"
+rm -rf /root/cysic-verifier/data/cysic-verifier.db
+
+sleep 2
+
+echo -e "${GREEN}Запуск ноди${NC}"
+cd /root/cysic-verifier && bash start.sh > /root/cysic-verifier/logs.txt 2>&1 &

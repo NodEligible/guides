a1fcf72dac56f3546fde535167eb3c9a0921973d
NodEligible <dudarvasya1999@gmail.com>
2025-01-28 19:17:40 +0100

    Create install.sh

---
+++ privasea/install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
+
+YELLOW='\e[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+  echo -e "${YELLOW}Установка Docker...${NC}"
+  bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/docker.sh)
+  if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Docker успешно установлено!${NC}"
+  else
+      echo -e "${RED}Ошибка при установке Docker!${NC}"
+  fi
+
+  echo -e "${YELLOW}Установка Main...${NC}"
+  bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/main.sh)
+  if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Main успешно установлен!${NC}"
+  else
+      echo -e "${RED}Ошибка при установке Main!${NC}"
+  fi
+
+  echo -e "${YELLOW}Установка Ufw...${NC}" 
+  bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/ufw.sh)
+  if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Ufw успешно установлено!${NC}"
+  else
+      echo -e "${RED}Ошибка при установке Ufw!${NC}"
+  fi
+
+echo -e "${YELLOW}Установка Privasea...${NC}"
+
+mkdir -p $HOME/privasea/config
+
+docker pull privasea/acceleration-node-beta:latest
+
+docker run -it -v "$HOME/privasea/config:/app/config"  privasea/acceleration-node-beta:latest ./node-calc new_keystore
+
+docker rm -f $(docker ps -a | grep privasea | awk '{print $1}') &>/dev/null
+
+mv $HOME/privasea/config/UTC-* $HOME/privasea/config/wallet_keystore
+
+echo -e "${GREEN}Установка завершена🚀${NC}"

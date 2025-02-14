5a963c9db9260abbfe3412107769fe6d2530ac61
NodEligible <dudarvasya1999@gmail.com>
2025-02-14 17:50:12 +0100

    Update update.sh

--- multiple/update.sh
+++ multiple/update.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
+
+YELLOW='\e[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${YELLOW}🔄 Обновление пакетов...${NC}"
+sudo apt update
+sudo apt install python3 python3-pip -y
+
+echo -e "${YELLOW}Установка Gdown...${NC}"
+pip install gdown
+
+
 cd $HOME/multipleforlinux
 
 # Отримання поточної версії

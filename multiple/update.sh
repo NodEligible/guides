ad1cb4749020554cebe7680b4e7b5b94f58129ca
NodEligible <dudarvasya1999@gmail.com>
2025-02-14 17:59:00 +0100

    Update update.sh

--- multiple/update.sh
+++ multiple/update.sh
@@ -28,24 +28,14 @@ echo -e "Текущая версия: $current_version"
 
     # Завантаження нового файлу
     gdown --id 1KRKoEyex7hyX5zXPDCJW_kP4G6XX5mNU -O multipleforlinux.tar
-
-    # Розпакування архіву
+fi
     tar -xvf multipleforlinux.tar
     rm -rf multipleforlinux.tar
 
-    # Оновлення прав
+    chmod -R 777 multipleforlinux
+    cd multipleforlinux
     chmod +x ./multiple-cli
     chmod +x ./multiple-node
 
-    # Перезапуск служби
     sudo systemctl start multiple
-
-    # Перевірка статусу служби
-    sudo systemctl is-active --quiet multiple
-    if [[ $? -ne 0 ]]; then
-        echo -e "${RED}Ошибка: Служба multiple не запустилась! Проверьте логи.${NC}"
-        exit 1
-    fi
-
-    echo -e "${GREEN}Обновление завершено!${NC}"
 fi

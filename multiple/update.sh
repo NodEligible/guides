1b493685a4af5724e9b08df085868835c4cc1550
NodEligible <dudarvasya1999@gmail.com>
2025-02-14 18:03:00 +0100

    Update update.sh

--- multiple/update.sh
+++ multiple/update.sh
@@ -14,13 +14,17 @@ sudo apt install python3 python3-pip -y
 echo -e "${YELLOW}Установка Gdown...${NC}"
 pip install gdown
 
-
 cd $HOME/multipleforlinux
 
 # Отримання поточної версії
 current_version=$(./multiple-cli --version)
 echo -e "Текущая версия: $current_version"
 
+# Отримання останньої версії
+latest_version=$(curl -s https://mdeck-download.s3.us-east-1.amazonaws.com/client/linux/version.txt)
+echo -e "Последняя версия: $latest_version"
+
+if [[ "$current_version" != "$latest_version" ]]; then
     echo -e "Обновление multiple node"
 
     # Зупинка служби
@@ -28,7 +32,8 @@ echo -e "Текущая версия: $current_version"
 
     # Завантаження нового файлу
     gdown --id 1KRKoEyex7hyX5zXPDCJW_kP4G6XX5mNU -O multipleforlinux.tar
-fi
+
+    # Розпакування архіву
     tar -xvf multipleforlinux.tar
     rm -rf multipleforlinux.tar
 
@@ -37,5 +42,9 @@ fi
     chmod +x ./multiple-cli
     chmod +x ./multiple-node
 
+    # Перезапуск служби
     sudo systemctl start multiple
+    echo -e "${GREEN}Обновление завершено!${NC}"
+else
+    echo -e "${GREEN}У вас уже последняя версия.${NC}"
 fi

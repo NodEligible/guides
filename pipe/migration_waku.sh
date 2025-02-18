bf395d2db8ff1aba9d4cf6d0f3c4d5ce7929ef6c
NodEligible <dudarvasya1999@gmail.com>
2025-02-18 12:55:22 +0100

    Update migration_waku.sh

--- pipe/migration_waku.sh
+++ pipe/migration_waku.sh
@@ -13,6 +13,7 @@ cd $HOME/nwaku-compose
 docker compose down
 
 sed -i 's|127.0.0.1:8003:8003|127.0.0.1:8333:8003|' $HOME/nwaku-compose/docker-compose.yml
+sed -i 's|- 80:80|- 1989:80|' $HOME/nwaku-compose/docker-compose.yml
 
 docker compose up -d
 

9dc2aea71fccfe6f7fe665cf387ed23a14160007
NodEligible <dudarvasya1999@gmail.com>
2025-02-21 21:23:22 +0100

    Update updates.sh

--- nexus/updates.sh
+++ nexus/updates.sh
@@ -8,18 +8,12 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 echo -e "${YELLOW}Обновление Nexus...${NC}"
-systemctl stop nexus
+source $HOME/.cargo/env
 
 cd $HOME/.nexus/network-api
-
-git fetch
+git stash
+git fetch --tags
 git -c advice.detachedHead=false checkout $(git rev-list --tags --max-count=1)
 
-cd $HOME/.nexus/network-api/clients/cli
-cargo build --release --bin prover
-
-rm -rf $HOME/.nexus/network-api/clients/cli/prover
-cp $HOME/.nexus/network-api/clients/cli/target/release/prover $HOME/.nexus/network-api/clients/cli/prover
-
-systemctl start nexus
+cd $HOME/.nexus/network-api/clients/cli && cargo run -r -- start --env beta
 echo -e "${GREEN}Обновление Nexus завершено${NC}"

5e1b86dd450c470a4dadf73635a80ff03b8182e2
NodEligible <dudarvasya1999@gmail.com>
2025-03-01 21:20:51 +0100

    Update delete_gaia_bot.sh

--- gaianet/delete_gaia_bot.sh
+++ gaianet/delete_gaia_bot.sh
@@ -1,14 +1,8 @@
 # Зупиняємо та вимикаємо сервіс бота
 sudo systemctl stop gaia-bot.service
 sudo systemctl disable gaia-bot.service
-
-# Видаляємо файл сервісу
 sudo rm -f /etc/systemd/system/gaia-bot.service
-
-# Перезапускаємо демон systemd, щоб прибрати сервіс
 sudo systemctl daemon-reload
-
-# Видаляємо папку з ботом і всі його файли
 rm -rf ~/gaia-bot
 
 

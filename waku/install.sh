99635f974a4d78df4500b6d60e075318eaa94773
NodEligible <dudarvasya1999@gmail.com>
2025-03-03 15:56:34 +0100

    Update install.sh

--- waku/install.sh
+++ waku/install.sh
@@ -80,7 +80,7 @@ setup_env() {
   sed -i 's/127\.0\.0\.1:4000:4000/0.0.0.0:4044:4000/g' $HOME/nwaku-compose/docker-compose.yml
   sed -i 's|127.0.0.1:8003:8003|127.0.0.1:8333:8003|' $HOME/nwaku-compose/docker-compose.yml
   sed -i 's/:5432:5432/:5444:5432/g' $HOME/nwaku-compose/docker-compose.yml
-
+  sed -i 's|- 80:80|- 1989:80|' $HOME/nwaku-compose/docker-compose.yml
 
   bash $HOME/nwaku-compose/register_rln.sh
 }

69e85c8cd5bc2c3fc0664afff4f0504969dd447b
NodEligible <dudarvasya1999@gmail.com>
2025-03-08 13:13:49 +0100

    Update install.sh

--- brinx/install.sh
+++ brinx/install.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
+
+# Сменные для цветов
+YELLOW='\e[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # Сброс цвета
+
+  echo -e "${YELLOW}Установка Docker...${NC}"
+  bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/docker.sh)
+  if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Docker успешно установлено!${NC}"
+  else
+      echo -e "${RED}Ошибка при установке Docker!${NC}"
+      exit 1
+  fi
+
+    echo -e "${YELLOW}Установка Main...${NC}"
+  bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/main.sh)
+  if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Main успешно установлен!${NC}"
+  else
+      echo -e "${RED}Ошибка при установке Main!${NC}"
+      exit 1
+  fi
+
+    echo -e "${YELLOW}Установка Ufw...${NC}" 
+  bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/ufw.sh)
+  if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Ufw успешно установлено!${NC}"
+  else
+      echo -e "${RED}Ошибка при установке Ufw!${NC}"
+      exit 1
+  fi
 
 # Скачиваем image из Docker Hub
 docker pull admier/brinxai_nodes-worker:latest

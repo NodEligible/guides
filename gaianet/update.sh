031c6e7d5f0189974566aad45eb3fcb7dd9bdc6d
NodEligible <dudarvasya1999@gmail.com>
2025-03-19 17:04:50 +0100

    Create update.sh

---
+++ gaianet/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
+
+YELLOW='\e[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${YELLOW}Делаем бекап GaiaNet...${NC}"

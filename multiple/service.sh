717f346b06315be7116c27f2b07ab913c0033c37
NodEligible <dudarvasya1999@gmail.com>
2025-04-08 04:04:06 +0200

    Update service.sh

--- multiple/service.sh
+++ multiple/service.sh
@@ -1,6 +1,6 @@
 # Check if both arguments are provided
 if [ $# -ne 2 ]; then
-    echo "Usage: bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/multiple/health.sh) <IDENTIFIER> <PIN>"
+    echo "Usage: bash <(curl -s https://raw.githubusercontent.com/NodEligible/guides/main/multiple/service.sh) <IDENTIFIER> <PIN>"
     exit 1
 fi
 
@@ -10,10 +10,10 @@ PIN="$2"
 while true; do
 STATUS_OUTPUT=$($HOME/multipleforlinux/multiple-cli status)
 if echo "$STATUS_OUTPUT" | grep -q " :False"; then
-    echo "Node is not running. Executing bind command..."
+    echo "Узел не запущен. Выполнение команды bind..."
     $HOME/multipleforlinux/multiple-cli bind --bandwidth-download 100 --identifier "$IDENTIFIER" --pin "$PIN" --storage 200 --bandwidth-upload 100
 else
-    echo "Node is already binded. NodeRun: True, IsMain: True"
+    echo "Узел уже привязан. NodeRun: True, IsMain: True"
 fi
     # Wait for 5 minutes before checking again
     sleep 300

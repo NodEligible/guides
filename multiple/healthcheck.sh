c23508c8e9dc19ac6035dcca82b86e3703d2d57f
NodEligible <dudarvasya1999@gmail.com>
2025-04-08 04:49:25 +0200

    Update healthcheck.sh

--- multiple/healthcheck.sh
+++ multiple/healthcheck.sh
@@ -1,4 +1,9 @@
-# Check if both arguments are provided
+
+YELLOW='\e[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
 if [ $# -ne 2 ]; then
     echo "Usage: bash <(curl -s https://raw.githubusercontent.com/NodEligible/guides/main/multiple/healthcheck.sh) <IDENTIFIER> <PIN>"
     exit 1
@@ -10,10 +15,10 @@ PIN="$2"
 while true; do
 STATUS_OUTPUT=$($HOME/multipleforlinux/multiple-cli status)
 if echo "$STATUS_OUTPUT" | grep -q " :False"; then
-    echo "Узел не запущен. Выполнение команды bind..."
+    echo -e "${RED}Узел не запущен. Выполнение команды bind...${NC}
     $HOME/multipleforlinux/multiple-cli bind --bandwidth-download 100 --identifier "$IDENTIFIER" --pin "$PIN" --storage 200 --bandwidth-upload 100
 else
-    echo "Узел уже привязан. NodeRun: True, IsMain: True"
+    echo -e "${GREEN}Узел уже привязан. NodeRun: True, IsMain: True${NC}"
 fi
     # Wait for 5 minutes before checking again
     sleep 300

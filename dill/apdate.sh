7b3478db6e1ba77f9dbdf81a77cceb552a4acabe
NodEligible <dudarvasya1999@gmail.com>
2025-04-11 19:52:33 +0200

    Update apdate.sh

--- dill/apdate.sh
+++ dill/apdate.sh
@@ -42,8 +42,7 @@ cd $HOME
 
 wget https://raw.githubusercontent.com/DillLabs/launch-dill-node/main/upgrade.sh -O $HOME/upgrade.sh
 chmod +x upgrade.sh
-DILL_DIR="$HOME/dill"
-sed -i "s|\./$DILL_DIR/1_start_dill_node.sh| |" "$HOME/upgrade.sh"
+sed -i "s|$DILL_DIR/1_start_dill_node.sh| |" "$HOME/upgrade.sh"
 ./upgrade.sh &>/dev/null
 rm -rf $HOME/upgrade.sh
 

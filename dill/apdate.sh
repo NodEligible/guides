7a43fdd319e9e9aa07ecf6a4d46352fade0a663f
NodEligible <dudarvasya1999@gmail.com>
2025-04-11 21:31:29 +0200

    Update apdate.sh

--- dill/apdate.sh
+++ dill/apdate.sh
@@ -8,24 +8,6 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-  echo -e "${YELLOW}Установка Ufw...${NC}" 
-  bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/ufw.sh)
-  if [ $? -eq 0 ]; then
-      echo -e "${GREEN}Ufw успешно установлено!${NC}"
-  else
-      echo -e "${RED}Ошибка при установке Ufw!${NC}"
-      exit 1
-  fi
-
-      echo -e "${YELLOW}Установка Main...${NC}"
-  bash <(curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/main.sh)
-  if [ $? -eq 0 ]; then
-      echo -e "${GREEN}Main успешно установлен!${NC}"
-  else
-      echo -e "${RED}Ошибка при установке Main!${NC}"
-      exit 1
-  fi
-
 echo -e "${YELLOW}Остановка сервиса${NC}"
 if [ -d "$HOME/dill" ]; then
     sudo systemctl stop dill 
@@ -43,38 +25,24 @@ cd $HOME
 # Завантажуємо скрипт оновлення Dill
 wget https://raw.githubusercontent.com/DillLabs/launch-dill-node/main/upgrade.sh -O $HOME/upgrade.sh
 chmod +x upgrade.sh
-
-# Видаляємо запуск 1_start_dill_node.sh, якщо він там є
-sed -i '/1_launch_dill_node.sh/d' "$HOME/upgrade.sh"
-
-# Запускаємо оновлення
+sed -i 's|\./start_dill_node\.sh| |' "$HOME/upgrade.sh"
 bash ./upgrade.sh
-
-# Прибираємо скрипт оновлення
 rm -f $HOME/upgrade.sh
 
-# Качаємо скрипт сервісника
+# Качаем скрипт с сервисником
 cd $HOME/dill
 curl -sO https://raw.githubusercontent.com/NodEligible/guides/main/dill/dill_service.sh
 chmod +x dill_service.sh
 
-# Міняємо дефолтні порти
+# Заменяем порты 
 sed -i 's|monitoring-port  9080 tcp|monitoring-port  8380 tcp|' "$HOME/dill/default_ports.txt"
 sed -i 's|exec-http.port 8545 tcp|exec-http.port 8945 tcp|' "$HOME/dill/default_ports.txt"
 sed -i 's|exec-port 30303 tcp|exec-port 30305 tcp|g; s|exec-port 30303 udp|exec-port 30305 udp|g' "$HOME/dill/default_ports.txt"
 
-# Замінюємо старий запуск на виклик dill_service.sh з параметрами
-sed -i 's|nohup \$PJROOT/\$NODE_BIN \$COMMON_FLAGS \$DISCOVERY_FLAGS \$VALIDATOR_FLAGS \$PORT_FLAGS > /dev/null 2>&1 &|\$PJROOT/dill_service.sh "\$PJROOT/\$NODE_BIN \$COMMON_FLAGS \$DISCOVERY_FLAGS \$VALIDATOR_FLAGS \$PORT_FLAGS"|' "$HOME/dill/start_dill_node.sh"
+# Заменяем нохап запуск на создание сервисника
+sed -i 's|nohup \$PJROOT/\$NODE_BIN \$COMMON_FLAGS \$DISCOVERY_FLAGS \$VALIDATOR_FLAGS \$PORT_FLAGS > /dev/null 2>&1 &|\$PJROOT/dill_service.sh \"\$PJROOT/\$NODE_BIN \$COMMON_FLAGS \$DISCOVERY_FLAGS \$VALIDATOR_FLAGS \$PORT_FLAGS\"|' "$HOME/dill/start_dill_node.sh"
 
-# Перезапускаємо ноду через systemd
-# 8. Перезапускаємо ноду через systemd
-echo -e "${YELLOW}🔁 Перезапускаємо Dill через systemd...${NC}"
-sudo systemctl daemon-reload
-
-if sudo systemctl restart dill; then
-  echo -e "${GREEN}✅ Dill нода успішно оновлена та перезапущена!${NC}"
-else
-  echo -e "${RED}❌ Помилка під час перезапуску Dill. Перевір: journalctl -u dill -e${NC}"
-fi
+# Запускаем скрипт по запуску ноды
+bash $HOME/dill/1_launch_dill_node.sh
 
 echo -e "${GREEN}Обновление завершено!${NC}"

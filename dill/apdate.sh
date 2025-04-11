5e5515957eb20e0844e31f23a526196dedd0a9c8
NodEligible <dudarvasya1999@gmail.com>
2025-04-11 19:20:58 +0200

    Update apdate.sh

--- dill/apdate.sh
+++ dill/apdate.sh
@@ -42,7 +42,7 @@ cd $HOME
 
 wget https://raw.githubusercontent.com/DillLabs/launch-dill-node/main/upgrade.sh -O $HOME/upgrade.sh
 chmod +x upgrade.sh
-sed -i 's|$DILL_DIR/1_launch_dill_node.sh| |' "$HOME/upgrade.sh"
+sed -i 's|$DILL_DIR/1_start_dill_node.sh| |' "$HOME/upgrade.sh"
 ./upgrade.sh &>/dev/null
 rm -rf $HOME/upgrade.sh
 
@@ -60,6 +60,6 @@ sed -i 's|exec-port 30303 tcp|exec-port 30305 tcp|g; s|exec-port 30303 udp|exec-
 sed -i 's|nohup \$PJROOT/\$NODE_BIN \$COMMON_FLAGS \$DISCOVERY_FLAGS \$VALIDATOR_FLAGS \$PORT_FLAGS > /dev/null 2>&1 &|\$PJROOT/dill_service.sh \"\$PJROOT/\$NODE_BIN \$COMMON_FLAGS \$DISCOVERY_FLAGS \$VALIDATOR_FLAGS \$PORT_FLAGS\"|' "$HOME/dill/start_dill_node.sh"
 
 # Запускаем скрипт для старта ноды
-bash $HOME/dill/1_launch_dill_node.sh
+bash $HOME/dill/1_start_dill_node.sh
 
 echo -e "${GREEN}Обновление завершено!${NC}"

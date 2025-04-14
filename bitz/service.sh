804b58f5b424dc04eddac95340c3d6ec476fd2a0
NodEligible <dudarvasya1999@gmail.com>
2025-04-14 02:33:28 +0200

    Update service.sh

--- bitz/service.sh
+++ bitz/service.sh
@@ -1,4 +1,15 @@
-sudo nano /etc/systemd/system/bitz.service
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
+
+YELLOW='\e[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${YELLOW}📝 Создание systemd-сервиса...${NC}"
+
+cat <<EOF > "/etc/systemd/system/bitz.service"
 [Unit]
 Description=Bitz Collect Miner Service
 After=network.target
@@ -12,8 +23,11 @@ LimitNOFILE=65535
 
 [Install]
 WantedBy=multi-user.target
+EOF
 
 
 sudo systemctl enable bitz
 sudo systemctl daemon-reload
 sudo systemctl start bitz
+
+echo -e "${GREEN}✅ Установка завершена!${NC}"

4b7b5c3caf5049f6b4511ab1d812612e37c8f38e
NodEligible <dudarvasya1999@gmail.com>
2025-04-17 05:36:02 +0200

    Update install.sh

--- drosera/install.sh
+++ drosera/install.sh
@@ -91,17 +91,17 @@ drosera apply
 echo -e "${GREEN}Trap настроен!${NC}"
 
 # -------------------------------------------------------------
-delay_minutes=15
-total_seconds=$((delay_minutes * 60))
+#delay_minutes=15
+#total_seconds=$((delay_minutes * 60))
 
-echo -e "\n⏳ Ожидание $delay_minutes минут..."
+#echo -e "\n⏳ Ожидание $delay_minutes минут..."
 
-for ((i=total_seconds; i>0; i--)); do
-    printf "\r⏱️  Осталось: %02d:%02d " $((i/60)) $((i%60))
-    sleep 1
-done
+#for ((i=total_seconds; i>0; i--)); do
+#    printf "\r⏱️  Осталось: %02d:%02d " $((i/60)) $((i%60))
+#    sleep 1
+#done
 
-echo -e "\n✅ Время вышло!"
+#echo -e "\n✅ Время вышло!"
 read -p "➡️  Нажмите Enter, чтобы продолжить..."
 
 # -------------------------------------------------------------

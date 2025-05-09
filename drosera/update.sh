35084a9ef1c1b7b26657a73f5570ebb4ab77abda
NodEligible <dudarvasya1999@gmail.com>
2025-05-09 12:48:59 +0200

    Update update.sh

--- drosera/update.sh
+++ drosera/update.sh
@@ -1,1 +1,31 @@
-12
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
+
+YELLOW='\e[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+BLUE='\033[38;5;81m'
+NC='\033[0m'
+
+echo -e "${YELLOW}Останавливаем drosera${NC}"
+service drosera stop
+source /root/.profile
+echo -e "${YELLOW}Обновляемм Drosera${NC}"
+curl -s -L https://app.drosera.io/install | bash
+droseraup 
+
+if [ ! -f drosera.toml ]; then
+  cd drosera
+fi
+echo -e "Обновляем drosera_rpc${NC}"
+sed -i 's|^drosera_rpc =.*|drosera_rpc = "https://relay.testnet.drosera.io"|' drosera.toml
+
+# get private key to do drosera apply with it
+SERVICE_FILE="/etc/systemd/system/drosera.service"
+private_key=$(grep 'ExecStart=' "$SERVICE_FILE" | sed -n 's/.*--eth-private-key \([^ ]*\).*/\1/p')
+
+drosera apply --private-key="$private_key"
+
+service drosera start
+echo -e "${GREEN}Обновление завершено!${NC}" 

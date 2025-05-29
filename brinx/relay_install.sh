08392867ddbb42f2f057ee2abc8722af05e771ed
NodEligible <dudarvasya1999@gmail.com>
2025-05-29 02:21:43 +0200

    Update relay_install.sh

--- brinx/relay_install.sh
+++ brinx/relay_install.sh
@@ -19,9 +19,11 @@ NC='\033[0m' # Сброс цвета
   fi
 
 echo -e "${YELLOW}Удаляем ноду если установлена...${NC}"
-docker stop brinxai_relay &>/dev/null
+docker stop watchtower
+docker stop brinxai_relay 
 docker rm -f brinxai_relay &>/dev/null
 docker rmi -f admier/brinxai_nodes-relay &>/dev/null
+docker rmi -f containrrr/watchtower &>/dev/null
 
 echo -e "${YELLOW}Установка BrixAI Relay${NC}"
 ARCH=$(dpkg --print-architecture)

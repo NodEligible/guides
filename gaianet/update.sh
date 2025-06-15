437d2d297b0ae3e4ed4c26fcb1e4795c20e50f6e
NodEligible <dudarvasya1999@gmail.com>
2025-06-15 23:08:28 +0200

    Update update.sh

--- gaianet/update.sh
+++ gaianet/update.sh
@@ -7,7 +7,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-
+echo -e "${YELLOW}Останавливаем gaia-bot...${NC}"
 sudo systemctl stop gaia-bot.service
 
 sleep 2
@@ -96,6 +96,8 @@ gaianet start
 sleep 10
 
 systemctl daemon-reload
+
+echo -e "${YELLOW}Запускаем gaia-bot${NC}"
 sudo systemctl start gaia-bot.service
 
 echo -e "${GREEN}Обновление завершено!${NC}"

fa83e5229d781ffa4c5b5a4943a030da774f8690
NodEligible <dudarvasya1999@gmail.com>
2025-07-18 12:50:58 +0200

    Update ritual_apdate_ports.sh

--- gensyn/ritual_apdate_ports.sh
+++ gensyn/ritual_apdate_ports.sh
@@ -15,17 +15,17 @@ CONTAINER_ID=$(docker ps -a --filter "ancestor=$IMAGE_NAME" --format "{{.ID}}")
 if [ -n "$CONTAINER_ID" ] && [ -f "$CONFIG_FILE" ]; then
     CONTAINER_NAME=$(docker inspect --format '{{.Name}}' "$CONTAINER_ID" | sed 's/^\/\?//')
 
-    echo -e"${YELLOW}Контейнер найден, останавливаем и удаляем:${NC} $CONTAINER_NAME"
+    echo -e "${YELLOW}Контейнер найден, останавливаем и удаляем:${NC} $CONTAINER_NAME"
     
     docker stop "$CONTAINER_NAME"
     docker rm "$CONTAINER_NAME"
     
-    echo -e"${YELLOW}Контейнер удален.${NC}"
+    echo -e "${YELLOW}Контейнер удален.${NC}"
 
     sed -i 's/3000/3009/g' "$CONFIG_FILE"
     echo -e "${GREEN}В файле${NC} $CONFIG_FILE: ${GREEN}заменили порт 3000 на 3009.${NC}"
 
-    echo -e"${YELLOW}Запускаем контейнер назад.${NC}"
+    echo -e "${YELLOW}Запускаем контейнер назад.${NC}"
     docker run -d --name "$CONTAINER_NAME" --restart unless-stopped -v "$CONFIG_FILE:/app/config.json" ritualnetwork/hello-world-infernet:latest
 
     echo -e "${GREEN}Готово.${NC}"

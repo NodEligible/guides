ef98adeebf725e6788732f82cfebcbe1352b7797
NodEligible <dudarvasya1999@gmail.com>
2025-10-09 20:14:13 +0200

    Update install_mainet.sh

--- pipe/install_mainet.sh
+++ pipe/install_mainet.sh
@@ -3,6 +3,8 @@
 # 🚀 PipeCDN Node Mainnet Auto-Installer
 # ================================================
 
+curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
+
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
